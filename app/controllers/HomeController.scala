8488521a4ac8fd37572fdd6a9763d2ed4e8aed19
Yoshihiro_Saito <yshr446@gmail.com>
2017-07-03 19:32:18 +0900

    top page

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,25 +1,17 @@
-package controllers
+ackage controllers
 
 import javax.inject._
-import play.api._
+
+import play.api.data.Form
+import play.api.data.Forms.{ optional, text }
+import play.api.i18n.{ I18nSupport, MessagesApi }
 import play.api.mvc._
 
-/**
-  * This controller creates an `Action` to handle HTTP requests to the
-  * application's home page.
-  */
 @Singleton
-class HomeController @Inject() extends Controller {
+class HomeController @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
-  /**
-    * Create an Action to render an HTML page.
-    *
-    * The configuration in the `routes` file means that this method
-    * will be called when the application receives a `GET` request with
-    * a path of `/`.
-    */
-  def index = Action { implicit request =>
-    Ok(views.html.index())
-  }
+ def index: Action[AnyContent] = Action { implicit request =>
+   Ok(views.html.index())
+ }
 
 }
